81716f01b050ec66b317b8493f7b00fcc10cdd7e
Unai Herrada <uherrada@aytosanlorenzo.es>
2026-04-17 12:37:35 +0200

    2.0.3

--- js/battleship.js
+++ js/battleship.js
@@ -1,4 +1,7 @@
 let seleccionando = false;
+let clicked_td;
+let unclicked_td;
+let celdasSeleccionadas = [];
 window.onload = iniciar;
 function iniciar() {
     let table = createTable(10, 10, true);
@@ -21,6 +24,7 @@ function createTable(columns, rows, withCoordinates) { // x horizontal y vertica
         let tr = document.createElement("tr");
         for (let y = 0; y < rows; y++) {
             let td = document.createElement("td");
+            td.id = xyToCoordinates(x, y, true);
             td.style.width = "25px";
             td.style.height = "25px";
             td.addEventListener("mouseup", cambiarColor)
@@ -40,28 +44,67 @@ function createTable(columns, rows, withCoordinates) { // x horizontal y vertica
 }
 
 function cambiarColor() {
-    console.log("d");
     seleccionando = false;
-    this.style.backgroundColor = "blue";
+    unclicked_td = this;
+    unclicked_td.style.backgroundColor = "blue";
     clicked_td.style.backgroundColor = "blue";
 }
 
 function seleccionandoCeldas() {
     seleccionando = true;
-    this.style.backgroundColor = "cyan";
-    console.log("c");
+    this.style.backgroundColor = "blue";
     clicked_td = this;
 }
 
 function celdaSeleccionada() {
-    console.log("a");
     if (seleccionando) {
-        console.log("b");
-        this.style.backgroundColor = "cyan";
+        if (this.style.backgroundColor == "") {
+            celdasSeleccionadas.push(this);
+            queCeldasEstanSeleccionadas(this);
+        }
     }
 }
 
+function queCeldasEstanSeleccionadas(last_td) {
+    let clicked_td_coords = [String.fromCharCode(65 - clicked_td.id[0]), clicked_td.id.slice(1) - 1];
+    let last_td_coords = [String.fromCharCode(65 - last_td.id[0]), last_td.id.slice(1) - 1];
+    if (clicked_td_coords[0] == last_td_coords[0]) {
+        for (let i = clicked_td_coords[1]; i = last_td_coords[1];) {
+            if (clicked_td_coords[1] < last_td_coords[1]) {
+                i++;
+                if (document.getElementById(xyToCoordinates())) {
+
+                }
+            } else {
+                i--;
+            }
+        }
+    } else if (clicked_td_coords[1] == last_td_coords[1]) {
+        for (let i = clicked_td_coords[0]; i = last_td_coords[0];) {
+            if (clicked_td_coords[0] < last_td_coords[0]) {
+                i++;
+                if (i) {
+
+                }
+            } else {
+                i--;
+            }
+        }
+    } else {
+        
+    }
+}
+
+function xyToCoordinates(x, y, reverseOrder) {
+    let xCoordinate = x + 1;
+    let yCoordinate = String.fromCharCode(65 + y);
+    if (reverseOrder)
+        return yCoordinate + xCoordinate;
+    return xCoordinate + yCoordinate;
+}
+
 /*
+cursor: not-allowed;
 en el popup quiero que el jugador escoja su posicion de los barcos
 y tambien la dificultad de a AI con un desplegable
 el desplegable marca el % de random de hacer el mejor movimiento
